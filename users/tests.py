b835e8353d9fb834f28bfee1ad517fe6b460a4be
Virgilio Angel González Rivas <virgilio.mx@gmail.com>
2020-06-01 01:39:49 -0500

    ch3

---
+++ users/tests.py
@@ -0,0 +1,32 @@
+# users/test.py
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="virgilio",
+            email="virgilio@email.com",
+            password="testpass123"
+        )
+        self.assertEqual(user.username, 'virgilio')
+        self.assertEqual(user.email, 'virgilio@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@email.com',
+            password='testpass123'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
